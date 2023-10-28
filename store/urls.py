9fc3f99414026d81c8b4cd4cd5e63421eacf3a9e
CaLIJaaa <000lobovsasha000@mail.ru>
2023-10-28 12:44:43 +0300

    load static in html

--- store/urls.py
+++ store/urls.py
@@ -5,5 +5,5 @@ from products.views import index, products
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('prod/', products, name='prpducts'),
+    path('prod/', products, name='products'),
 ]
